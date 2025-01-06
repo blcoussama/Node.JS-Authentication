e5aa52c1e007e859aa1fdbb167c1416ccf14ccff
blcoussama <belcadioussama.eng.com>
2025-01-06 11:13:50 +0100

    Completed SignUp UI and password Criteria Functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AppLayout from './components/AppLayout'
 import HomePage from './pages/HomePage'
 import SignUp from './pages/SignUp'
+import Login from './pages/Login'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
           <Routes>
             <Route path="/" element={ <HomePage /> } />
             <Route path="/signup" element={ <SignUp /> } />
+            <Route path="/login" element={ <Login /> } />
           </Routes>
         </AppLayout>
       </BrowserRouter>
