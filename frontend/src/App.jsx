fecc55e8a152b527ab8b37b8a5a030cbb3078230
blcoussama <belcadioussama.eng.com>
2025-01-06 12:06:21 +0100

    Email Verification form UI & Handling of Typed OR Pasted Code Auto Submition

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import AppLayout from './components/AppLayout'
 import HomePage from './pages/HomePage'
 import SignUp from './pages/SignUp'
 import Login from './pages/Login'
+import EmailVerification from './pages/EmailVerification'
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
             <Route path="/" element={ <HomePage /> } />
             <Route path="/signup" element={ <SignUp /> } />
             <Route path="/login" element={ <Login /> } />
+            <Route path="/email-verification" element={ <EmailVerification /> } />
           </Routes>
         </AppLayout>
       </BrowserRouter>
