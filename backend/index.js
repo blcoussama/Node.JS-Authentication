89342bebe264b92f338d38852698ec1fe666e541
blcoussama <belcadioussama.eng.com>
2025-01-08 08:28:14 +0100

    Setting the production environement and build process and run both the frontend and backend in one server: 3000

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import { connectDB } from './db/connectDB.js'
 import AuthRoute from './routes/authRoute.js'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import path from 'path'
 
 dotenv.config()
 
@@ -11,12 +12,22 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
+const __dirname = path.resolve()
+
 app.use(cors({ origin: "http://localhost:5173", credentials: true}))
 
 app.use(express.json()) // to parse JSON requests
 app.use(cookieParser()) // to parse incoming cookies
 
 app.use("/api/auth", AuthRoute)
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     connectDB()
